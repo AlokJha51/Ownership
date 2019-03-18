ff1a406900e03fcf596224454e1716e903e1135c
AlokJha51 <alok.jha@knoldus.in>
2019-03-18 12:30:48 +0530

    added updates

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-
+/// This function reurns middle names of a full  name
 fn middle_name(full_name: &str) -> &str {
+
     full_name.split_whitespace().nth(1).unwrap()
 }
-
+/// This function prints the middle name of pawan
 fn main() {
     let result: &str;
-    {
-        let name: String = String::from("Pawan Singh Bisht");
+
+        let name: String = String::from("Pawan Singh Bhist");
         result = middle_name(&name);
+
+    println!("{}",result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn middle_name_isvalid(){
+        assert_eq!("kumar",middle_name("Alok kumar jha"))
     }
-    assert_eq!(result, "Singh");
+    #[test]
+    #[should_panic]
+    fn name_not_valid() {
+        middle_name("Pawan");
+    }
+
+    #[test]
+    fn middle_name_is_not_valid(){
+        assert_ne!("singh",middle_name("Alok kumar jha"))
+    }
 }
